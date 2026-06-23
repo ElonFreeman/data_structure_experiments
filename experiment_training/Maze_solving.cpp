ef16facc75c589b21d887406b8c2250acf2e6d82
ElonFreeman <zhanghao112358@outlook.com>
2026-06-23 20:29:16 +0800

    26062304

--- experiment_training/Maze_solving.cpp
+++ experiment_training/Maze_solving.cpp
@@ -1,35 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int width_maze,height_maze;
-pair<int,int> enter,out;
-vector<vector<int>> maze_map;
+int width_maze,height_maze; //set the width and height of maze
+pair<int,int> enter,out; //entrance and exit
+vector<vector<int>> maze_map; //map of maze
 vector<pair<int,int>> path_stack,path_bfs;//DFS path stack and BFS reverse path
-deque<pair<int,int>> path_queue;//BFS
+deque<pair<int,int>> path_queue;//BFS path queue
 vector<vector<pair<int,int>>> parent;//path reverse,record parent position
 
 /*paraments:row,col:enter position*/
 bool findpath_DFS(int row,int col)
 {
-    if((row<0 || col<0 || row>=height_maze || col>=width_maze) || (maze_map[row][col]==1 || maze_map[row][col]==2))
+    if((row<0 || col<0 || row>=height_maze || col>=width_maze) || (maze_map[row][col]==1 || maze_map[row][col]==2)) //if current is out of boundary
     {return false;}
-    if(row==out.first && col==out.second)
+    if(row==out.first && col==out.second) //if current is exit
     {
-        pair<int,int> back; back.first=row,back.second=col; path_stack.push_back(back);
+        pair<int,int> back={row,col}; path_stack.push_back(back); //push into path stack
         return true;
     }
 
     maze_map[row][col]=2; //accessed lable
-    pair<int,int> back; back.first=row,back.second=col; path_stack.push_back(back); //push path stack
+    pair<int,int> back={row,col}; path_stack.push_back(back); //push into path stack
 
-    int dx[4] = {-1, 1, 0, 0},dy[4] = {0, 0, -1, 1};
-    for(int i=0;i<4;i++)
+    int dx[4] = {-1, 1, 0, 0},dy[4] = {0, 0, -1, 1}; //neighbor position
+    for(int i=0;i<4;i++) //traverse neighbors gracefully
     {
         int cx=row+dx[i],cy=col+dy[i]; if(findpath_DFS(cx,cy)) {return true;}
     }
 
-    path_stack.pop_back();
-    return false;
+    path_stack.pop_back(); //all neighbors traversed,not suitable
+    return false;  //pop out current
 }
 
 void findpath_BFS(int row,int col)
@@ -37,28 +37,28 @@ void findpath_BFS(int row,int col)
     pair<int,int> current={row,col}; path_queue.push_back(current); maze_map[row][col]=2; parent[row][col] = {row, col};
     while(!path_queue.empty())
     {
-        current=path_queue.front(); path_queue.pop_front();
+        current=path_queue.front(); path_queue.pop_front(); //get head element then pop out
         if(current.first==out.first && current.second==out.second)  //found out
         {
             pair<int,int> cur=out;
-            while(!(cur.first==enter.first && cur.second==enter.second))
+            while(!(cur.first==enter.first && cur.second==enter.second)) //if current is entrace
             {
-                path_bfs.push_back(cur); cur=parent[cur.first][cur.second];
+                path_bfs.push_back(cur); cur=parent[cur.first][cur.second]; //push current into path stack and refresh current to its parent
             }
-            path_bfs.push_back(enter);
-            reverse(path_bfs.begin(),path_bfs.end());
-
-            break;
+            path_bfs.push_back(enter); //push entrace into path stack
+            reverse(path_bfs.begin(),path_bfs.end()); //reverse the position of path stack
+            
+            break; //break outof cycle
         }
 
-        int dx[4] = {-1, 1, 0, 0},dy[4] = {0, 0, -1, 1};
-        for(int i=0;i<4;i++)
+        int dx[4] = {-1, 1, 0, 0},dy[4] = {0, 0, -1, 1}; //neighbor position
+        for(int i=0;i<4;i++) //traverse neighbors gracefully
         {
             int cx=current.first+dx[i],cy=current.second+dy[i];
-            if((cx>=0 && cy>=0 && cx<height_maze && cy<width_maze) && (maze_map[cx][cy]==0))
+            if((cx>=0 && cy>=0 && cx<height_maze && cy<width_maze) && (maze_map[cx][cy]==0)) //if current is out of boundary
             {
-                pair<int,int> neighbor={cx,cy}; path_queue.push_back(neighbor); 
-                maze_map[cx][cy]=2; parent[cx][cy]=current; 
+                pair<int,int> neighbor={cx,cy}; path_queue.push_back(neighbor); //push current into path stack
+                maze_map[cx][cy]=2; parent[cx][cy]=current; //record its parent
             }
         }
     }
@@ -98,17 +98,17 @@ void findpath_BFS(int row,int col)
 int main(void)
 {
     cin >> width_maze >> height_maze;
-    maze_map.assign(height_maze,vector<int>(width_maze,0)); parent.assign(height_maze,vector<pair<int,int>>(width_maze,{0,0}));
+    maze_map.assign(height_maze,vector<int>(width_maze,0)); parent.assign(height_maze,vector<pair<int,int>>(width_maze,{0,0}));  //resize map and parent recorder
     
-    for(int row=0;row<height_maze;row++)
+    for(int row=0;row<height_maze;row++) //input map of maze
     {
         for(int col=0;col<width_maze;col++)
         {
             cin >> maze_map[row][col];
         }
     }
-    vector<vector<int>> maze_map_backup=maze_map;
-    cin >> enter.first >> enter.second >> out.first >> out.second; //first:row,second:col
+    vector<vector<int>> maze_map_backup=maze_map; //backup of map,used to reset map after DFS
+    cin >> enter.first >> enter.second >> out.first >> out.second; //setup entrance and exit,first:row,second:col
 
     findpath_DFS(enter.first,enter.second);
     for(auto output:path_stack) {cout <<'(' << output.first << ',' << output.second << ')' << ' ';} cout << endl << endl;
