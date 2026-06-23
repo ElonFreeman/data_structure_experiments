e693b8f8e5ab98803ff91321deef5d66705fef46
ElonFreeman <zhanghao112358@outlook.com>
2026-06-23 11:51:53 +0800

    26062302

--- experiment_training/Maze_problem.cpp
+++ experiment_training/Maze_problem.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int width_maze,height_maze;
 pair<int,int> enter,out;
 vector<vector<int>> maze_map;
-vector<vector<pair<int,int>>> parent;
-vector<pair<int,int>> path_stack;//DFS
+vector<pair<int,int>> path_stack,path_bfs;//DFS path stack and BFS reverse path
 deque<pair<int,int>> path_queue;//BFS
-vector<vector<pair<int,int>>> parent;//path recursive,record parent position
+vector<vector<pair<int,int>>> parent;//path reverse,record parent position
 
+/*paraments:row,col:enter position*/
 bool findpath_DFS(int row,int col)
 {
-    if((row<0 || col<0 || row>height_maze || col>width_maze) || (maze_map[row][col]==1 || maze_map[row][col]==2))
+    if((row<0 || col<0 || row>=height_maze || col>=width_maze) || (maze_map[row][col]==1 || maze_map[row][col]==2))
     {return false;}
-    if(row==enter.first && enter.second)
+    if(row==out.first && col==out.second)
     {
         pair<int,int> back; back.first=row,back.second=col;
         path_stack.push_back(back);
@@ -32,46 +32,48 @@ bool findpath_DFS(int row,int col)
     return false;
 }
 
-
 void findpath_BFS(int row,int col)
 {
-    pair<int,int> current; current.first=row,current.second=col; path_queue.push_back(current); maze_map[row][col]=2;
+    pair<int,int> current; current.first=row,current.second=col; path_queue.push_back(current); maze_map[row][col]=2; parent[row][col] = {row, col};
     while(!path_queue.empty())
     {
         current=path_queue.front(); path_queue.pop_front();
-        if(current.first==out.first && current.second==out.second){break;}  //found out
+        if(current.first==out.first && current.second==out.second)  //found out
+        {
+            pair<int,int> cur=out;
+            while(!(cur.first==enter.first && cur.second==enter.second))
+            {
+                path_bfs.push_back(cur); cur=parent[cur.first][cur.second];
+            }
+            path_bfs.push_back(enter);
+            reverse(path_bfs.begin(),path_bfs.end());
+
+            break;
+        }
 
         //north
-        if((current.first-1<0 || current.second<0 || current.first-1>height_maze || current.second>width_maze) || (maze_map[current.first-1][current.second]==1 || maze_map[current.first-1][current.second]==2))
-        {continue;}
-        else
+        if((current.first-1>=0 && current.second>=0 && current.first-1<height_maze && current.second<width_maze) && (maze_map[current.first-1][current.second]!=1 && maze_map[current.first-1][current.second]!=2))
         {
             pair<int,int> neighbor; neighbor.first=current.first-1,neighbor.second=current.second; path_queue.push_back(neighbor);
             maze_map[neighbor.first][neighbor.second]=2;
             parent[neighbor.first][neighbor.second].first=current.first,parent[neighbor.first][neighbor.second].second=current.second;
         }
         //east
-        if((current.first<0 || current.second+1<0 || current.first>height_maze || current.second+1>width_maze) || (maze_map[current.first][current.second+1]==1 || maze_map[current.first][current.second+1]==2))
-        {continue;}
-        else
+        if((current.first>=0 && current.second+1>=0 && current.first<height_maze && current.second+1<width_maze) && (maze_map[current.first][current.second+1]!=1 && maze_map[current.first][current.second+1]!=2))
         {
             pair<int,int> neighbor; neighbor.first=current.first,neighbor.second=current.second+1; path_queue.push_back(neighbor);
             maze_map[neighbor.first][neighbor.second]=2;
             parent[neighbor.first][neighbor.second].first=current.first,parent[neighbor.first][neighbor.second].second=current.second;
         }
         //south
-        if((current.first+1<0 || current.second<0 || current.first+1>height_maze || current.second>width_maze) || (maze_map[current.first+1][current.second]==1 || maze_map[current.first+1][current.second]==2))
-        {continue;}
-        else
+        if((current.first+1>=0 && current.second>=0 && current.first+1<height_maze && current.second<width_maze) && (maze_map[current.first+1][current.second]!=1 && maze_map[current.first+1][current.second]!=2))
         {
             pair<int,int> neighbor; neighbor.first=current.first+1,neighbor.second=current.second; path_queue.push_back(neighbor);
             maze_map[neighbor.first][neighbor.second]=2;
             parent[neighbor.first][neighbor.second].first=current.first,parent[neighbor.first][neighbor.second].second=current.second;
         }
         //west
-        if((current.first<0 || current.second-1<0 || current.first>height_maze || current.second-1>width_maze) || (maze_map[current.first][current.second-1]==1 || maze_map[current.first][current.second-1]==2))
-        {continue;}
-        else
+        if((current.first>=0 && current.second-1>=0 && current.first<height_maze && current.second-1<width_maze) && (maze_map[current.first][current.second-1]!=1 && maze_map[current.first][current.second-1]!=2))
         {
             pair<int,int> neighbor; neighbor.first=current.first,neighbor.second=current.second-1; path_queue.push_back(neighbor);
             maze_map[neighbor.first][neighbor.second]=2;
@@ -101,9 +103,10 @@ int main(void)
     cin >> enter.first >> enter.second >> out.first >> out.second; //first:row,second:col
 
     findpath_DFS(enter.first,enter.second);
-    for(pair output:path_stack) {cout <<'(' << output.first << ',' << output.second << ')' << ' ';} cout << endl;
+    for(auto output:path_stack) {cout <<'(' << output.first << ',' << output.second << ')' << ' ';} cout << endl << endl;
     maze_map=maze_map_backup; 
     findpath_BFS(enter.first,enter.second);
+    for(auto output:path_bfs) {cout <<'(' << output.first << ',' << output.second << ')' << ' ';} cout << endl << endl;
     maze_map=maze_map_backup; 
 
     
